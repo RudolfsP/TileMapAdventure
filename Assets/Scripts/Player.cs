a90e890757a98b9d42250a82e0820f5966218cbe
RudolfsP <52312006+RudolfsP@users.noreply.github.com>
2021-01-28 22:21:39 +0200

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,21 +6,43 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
 
+    //Config
     [SerializeField] float runSpeed = 5f;
+
+    //State
+    bool isAlive = true;
+
+    //Cached component references
     Rigidbody2D myRigidBody;
-    // Start is called before the first frame update
+    Animator myAnimator;
+    
+    //Message then method
     void Start() {
         myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update() {
         Run();
+        FlipSprite();
     }
 
     private void Run() {
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");   //value between -1 to +1
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", playerHasHorizontalSpeed);
+    }
+
+    private void FlipSprite() {
+        
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+
+        if(playerHasHorizontalSpeed){
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+        }
     }
 }
