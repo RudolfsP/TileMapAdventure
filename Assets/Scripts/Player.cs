660d3450418d74de2802f74f9e1c9786bf2ff9db
RudolfsP <52312006+RudolfsP@users.noreply.github.com>
2021-03-09 21:41:16 +0200

    Cameras and no double jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,14 +17,16 @@ public class Player : MonoBehaviour {
     //Cached component references
     Rigidbody2D myRigidBody;
     Animator myAnimator;
-    Collider2D myCollider2D;
+    CapsuleCollider2D myBodyCollider2D;
+    BoxCollider2D myFeetCollider;
     float gravityScaleAtStart;
 
     //Message then method
     void Start() {
         myRigidBody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
-        myCollider2D = GetComponent<Collider2D>();
+        myBodyCollider2D = GetComponent<CapsuleCollider2D>();
+        myFeetCollider = GetComponent<BoxCollider2D>();
         gravityScaleAtStart = myRigidBody.gravityScale;
     }
 
@@ -49,7 +51,7 @@ public class Player : MonoBehaviour {
 
     private void Jump() {
 
-        if (!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
+        if (!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
 
         if (CrossPlatformInputManager.GetButtonDown("Jump")) {
             Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
@@ -58,7 +60,7 @@ public class Player : MonoBehaviour {
     }
 
     private void ClimbLadder() {
-        if(!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Climbing"))) {
+        if(!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Climbing"))) {
             myAnimator.SetBool("Climbing", false);
             myRigidBody.gravityScale = gravityScaleAtStart;
             return;
