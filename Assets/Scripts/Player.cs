a77edc8f359aaaf79440e63ad5664c95eb4b5dc8
RudolfsP <52312006+RudolfsP@users.noreply.github.com>
2021-03-25 17:38:10 +0200

    Hazards & levels
    Added hazards + multiple levels

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,6 +10,7 @@ public class Player : MonoBehaviour {
     [SerializeField] float runSpeed = 5f;
     [SerializeField] float jumpSpeed = 10f;
     [SerializeField] float climbSpeed = 5f;
+    [SerializeField] Vector2 deathKick = new Vector2(10f, 10f);
 
     //State
     bool isAlive = true;
@@ -32,10 +33,12 @@ public class Player : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+        if (!isAlive) { return; }
         Run();
         Jump();
         ClimbLadder();
         FlipSprite();
+        Die();
     }
 
     private void Run() {
@@ -74,6 +77,14 @@ public class Player : MonoBehaviour {
         bool playerHasVerticalSpeed = Mathf.Abs(myRigidBody.velocity.y) > Mathf.Epsilon;
         myAnimator.SetBool("Climbing", playerHasVerticalSpeed);
 
+    }
+
+    private void Die() {
+        if (myBodyCollider2D.IsTouchingLayers(LayerMask.GetMask("Enemy", "Hazards"))) {
+            isAlive = false;
+            myAnimator.SetTrigger("Dying");
+            GetComponent<Rigidbody2D>().velocity = deathKick;
+        }
     }
 
     private void FlipSprite() {
