3b8aa8f2f5c0e6f4ee4222a96f5831cc3d48b9ea
RudolfsP <52312006+RudolfsP@users.noreply.github.com>
2021-03-29 21:45:08 +0300

    Added menus and flow

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour {
+
+    [SerializeField] float waitTime = 2.5f;
+    [SerializeField] float levelExitSlowMoFactor = 0.2f;
+
+    private void OnTriggerEnter2D(Collider2D otherCollider) {
+        StartCoroutine(LoadNextScene());
+    }
+
+    IEnumerator LoadNextScene() {
+
+        Time.timeScale = levelExitSlowMoFactor;
+        //yield with a delay
+        yield return new WaitForSecondsRealtime(waitTime);
+        Time.timeScale = 1f;
+
+        //load next scene
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
