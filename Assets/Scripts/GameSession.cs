b7e4e1bd23c9ebe17518f6c082d0939779848159
RudolfsP <52312006+RudolfsP@users.noreply.github.com>
2021-03-30 17:39:59 +0300

    Persistent item pickups

---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameSession : MonoBehaviour {
+
+    [SerializeField] int playerLives = 3;
+    [SerializeField] int score = 0;
+    [SerializeField] Text livesText;
+    [SerializeField] Text scoreText;
+
+
+    private void Awake() {
+        //Creating a singleton.. or an object that destroys itself if it already exists, so there can only be one
+        int numGameSessions = FindObjectsOfType<GameSession>().Length;
+
+        if(numGameSessions > 1) {
+            Destroy(gameObject);
+        }
+
+        else {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void Start() {
+        livesText.text = playerLives.ToString();
+        scoreText.text = score.ToString();
+    }
+
+    public void AddToScore(int scoreToAdd) {
+        score += scoreToAdd;
+        scoreText.text = score.ToString();
+    }
+
+    public void ProcessPlayerDeath() {
+        if(playerLives > 1) {
+            TakeLife();
+        }
+
+        else {
+            ResetGameSession();
+        }
+    }
+
+    private void ResetGameSession() {
+        SceneManager.LoadScene(0);
+        Destroy(gameObject);
+    }
+
+    private void TakeLife() {
+        playerLives--;
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+        livesText.text = playerLives.ToString();
+    }
+}
